a89cdff3ff38b0fe84dfc34fa2c91a5ee61855eb
MatthewZGong <matthewzgong@gmail.com>
2023-10-06 14:03:03 -0400

    Initial Commit

---
+++ SDLInClass/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLInClass
+//
+//  Created by Matthew Gong on 10/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
